2c57561bdd41f64e522585ee463df854d9dfc83f
Dimitrios C. Michalakos <dimitris@jmike.gr>
2024-01-04 21:12:41 +0200

    feat: export fileMigrationHistoryLog

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 export * from './models';
 export * from './ports';
 export * from './adapters/fileMigrationRepo';
+export * from './adapters/fileMigrationHistoryLog';
 export * from './commands';
 export * from './errors';
